7f3239e04a588d3ddf5d766fcc8d98760937c3cd
Aaron Kincer <94083098+BashPink@users.noreply.github.com>
2022-07-07 11:31:59 -0400

    Update shutdown-listener.py
    Add comments to explain code better

--- shutdown-listener.py
+++ shutdown-listener.py
@@ -6,8 +6,10 @@ import subprocess
 # To disable warnings that pop up that don't seem to impact function but are anoyying.
 GPIO.setwarnings(False)
 
+# Pin and Ground listener for buttonn push.
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.wait_for_edge(3, GPIO.FALLING)
 
+# Command upon button push
 subprocess.call(['shutdown', '-h', 'now'], shell=False)
