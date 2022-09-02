34d60ea0f54b329765afe8951f4778ed034b8162
Aaron Kincer <94083098+BashPink@users.noreply.github.com>
2022-09-02 15:31:58 -0400

    Update shutdown-listener.py
    Update to allow OLED control

--- shutdown-listener.py
+++ shutdown-listener.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 
+#import OLED_SCRIPT
+import time
 import RPi.GPIO as GPIO
 import subprocess
 
-# To disable warnings that pop up that don't seem to impact function but are anoyying.
+# To disable warnings that pop up that don't seem to impact function but are anoying.
 GPIO.setwarnings(False)
 
-# Pin and Ground listener for buttonn push.
+# Pin and Ground listener for buttonn pushes
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)                 #Pins for I/O Button
 GPIO.wait_for_edge(3, GPIO.FALLING)
 
-# Command upon button push
-subprocess.call(['shutdown', '-h', 'now'], shell=False)
+# Pins to be added for OLED Functions
+#GPIO.setup(Null, GPIO.IN, pull_up_down=gpio.PUD_UP)             #Pins for OLED I/O
+#GPIO.wait_for_egde(NUll, GPIO.FALLING)
+
+# Command upon button pushes
+subprocess.call(['shutdown', '-h', 'now'], shell=False)          #Command for I/O
+#subprocess.call(['sudo', 'python', 'OLED_SCRIPT'], shell=False) #Command for OLED  
