67ec5e82225a6658a856f3c7d20ca601e13568d4
Aaron Kincer <94083098+BashPink@users.noreply.github.com>
2022-12-19 14:27:21 -0500

    Name Update and code Cleanup

--- Spyglass_PC.py
+++ Spyglass_PC.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-#import OLED_SCRIPT
 import time
 import RPi.GPIO as GPIO
 import subprocess
@@ -13,10 +12,8 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)                 #Pins for I/O Button
 GPIO.wait_for_edge(3, GPIO.FALLING)
 
-# Pins to be added for OLED Functions
 #GPIO.setup(Null, GPIO.IN, pull_up_down=gpio.PUD_UP)             #Pins for OLED I/O
 #GPIO.wait_for_egde(NUll, GPIO.FALLING)
 
 # Command upon button pushes
-subprocess.call(['shutdown', '-h', 'now'], shell=False)          #Command for I/O
-#subprocess.call(['sudo', 'python', 'OLED_SCRIPT'], shell=False) #Command for OLED  
+subprocess.call(['shutdown', '-h', 'now'], shell=False)          #Command for I/O  
